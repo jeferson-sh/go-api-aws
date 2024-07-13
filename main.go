33d5b5a0483db393059928c55216f01955507021
jeferson <jefersoun.silva@gmail.com>
2024-07-13 18:11:11 -0300

    feat: find address

--- main.go
+++ main.go
@@ -1,23 +1,70 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
-func messageHandler(w http.ResponseWriter, r *http.Request) {
-	// Defina o cabeçalho Content-Type para indicar que estamos retornando texto simples
-	w.Header().Set("Content-Type", "text/plain")
+type Address struct {
+	CEP         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	UF          string `json:"uf"`
+	IBGE        string `json:"ibge"`
+	GIA         string `json:"gia"`
+	DDD         string `json:"ddd"`
+	SIAFI       string `json:"siafi"`
+}
 
-	// Escreva a mensagem de "Olá Mundo!" no corpo da resposta
-	fmt.Fprintln(w, "Olá Mundo! Estamos na AWS!!!!")
+type ErrorResponse struct {
+	Message string `json:"message"`
+}
+
+func addressHandler(w http.ResponseWriter, r *http.Request) {
+	cep := r.URL.Query().Get("cep")
+	if cep == "" {
+		http.Error(w, "CEP is required", http.StatusBadRequest)
+		return
+	}
+
+	resp, err := http.Get(fmt.Sprintf("https://viacep.com.br/ws/%s/json/", cep))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(ErrorResponse{Message: "Failed to fetch address"})
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(ErrorResponse{Message: "Failed to read response body"})
+		return
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(ErrorResponse{Message: string(body)})
+		return
+	}
+
+	var address Address
+	if err := json.Unmarshal(body, &address); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(ErrorResponse{Message: "Failed to parse address"})
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(address)
 }
 
 func main() {
-	// Registre o manipulador de rota para o caminho "/message"
-	http.HandleFunc("/", messageHandler)
-
-	// Inicie o servidor na porta 8080
+	http.HandleFunc("/address", addressHandler)
 	fmt.Println("Servidor iniciado. Acesse http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
